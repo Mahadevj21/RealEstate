f89b3d262fe677e76cdfc2df4ea7cb04f2c8ef61
Mahadevj21 <mahadevjangid21@gmail.com>
2026-05-04 00:06:47 +0530

    chore: clean up root directory by moving local DB tools to hidden folder

--- create_db.js
+++
@@ -1,6 +0,0 @@
-const { Client } = require('pg');
-const c = new Client({ user: 'postgres', host: 'localhost', database: 'postgres', port: 5432 });
-c.connect()
-  .then(() => c.query('CREATE DATABASE "RealEstate"'))
-  .then(() => { console.log('DB created!'); c.end(); })
-  .catch(e => { console.log(e.message); c.end(); });
